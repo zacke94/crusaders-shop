a79ed7ead7764d6119656f3421093e3199b31802
adamerlandsson <adam.erlandsson@softhouse.se>
2025-12-24 00:16:28 +0100

    Added redirect button to startpage after order

--- crusaders-webshop/src/views/SuccessfulOrderView.vue
+++ crusaders-webshop/src/views/SuccessfulOrderView.vue
@@ -3,29 +3,40 @@
     <h1>Tack för ditt köp!</h1>
     <br />
 
-    <h3>Tänk på att att vi kameraövervakar, och märker om ni INTE betalar</h3>
-    <br />
+    <h2>Tänk på att att vi kameraövervakar, och märker om ni INTE betalar</h2>
 
-    <span class="progress-bar-seconds"
-      >Du blir omdirigeras till startsidan om {{ seconds }} sekunder...</span
-    >
+    <Button class="mt-32 center-div" label="Till startsidan" @click="onClickGoHome"></Button>
+    <h3 class="mt-16">
+      <span class="progress-bar-seconds"
+        >Eller bli automatiskt omdirigeras till startsidan om {{ seconds }} sekunder...</span
+      >
+    </h3>
   </div>
 </template>
 
 <script>
 import router from '@/router';
+import Button from 'primevue/button';
 
 export default {
   name: 'successful-order-view',
+  components: {
+    Button
+  },
   data() {
     return {
       seconds: 5
     };
   },
+  methods: {
+    onClickGoHome() {
+      router.replace({ path: '/' });
+    }
+  },
   mounted() {
     let timerId;
 
-    if (this.seconds > 0) {
+    /*if (this.seconds > 0) {
       timerId = setInterval(() => {
         this.seconds--;
         if (this.seconds <= 0) {
@@ -33,7 +44,15 @@ export default {
           router.replace({ path: '/' });
         }
       }, 1000);
-    }
+    }*/
   }
 };
 </script>
+
+<style scoped>
+.center-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
